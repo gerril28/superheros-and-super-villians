8f33f8adeb7bb2ad9384b215ea0627e489d2d9ff
gerril28 <113910059+gerril28@users.noreply.github.com>
2022-11-09 09:26:50 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,21 @@
 input.onButtonPressed(Button.A, function () {
     index = randint(0, 4)
     basic.showString("" + (list[index]))
-    list = p1
+    index = p1
 })
 input.onButtonPressed(Button.AB, function () {
     if (list[p1] == list[p1]) {
         basic.showString("" + (list[p1]))
         basic.showString("Win")
-        p1 = randint(0, 4)
     }
 })
 input.onButtonPressed(Button.B, function () {
-    let p2: string[] = []
+    let p2 = 0
     index = randint(0, 4)
     basic.showString("" + (Villians[index]))
-    Villians = p2
+    index = p2
 })
-let p1: string[] = []
+let p1 = 0
 let index = 0
 let Villians: string[] = []
 let list: string[] = []
